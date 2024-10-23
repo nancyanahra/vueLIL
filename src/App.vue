b54a2bdff30150631f831af813cfcb6b085ac232
nancyanahra <nancy.t.nahra@gmail.com>
2024-10-23 13:38:27 -0500

    chapter 7_1 beginning. Remove HellowWorld.Vue, About.vue, create a checkout.vue, fix routes, and scaffold components/views

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <div class="container"></div>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
+$primary: #6f42c1;
+
+app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +29,37 @@ nav {
       color: #42b983;
     }
   }
+}
+
+@import "node_modules/bootstrap/scss/bootstrap";
+.dropdown-clip {
+  overflow: hidden;
+}
+
+.dropdown-enter-active,
+.dropdown-leave-active {
+  transition: all 0.5s ease-in-out;
+  transform: auto;
+}
+
+.dropdown-enter-from,
+.dropdown-leave-to {
+  opacity: 0;
+  transform: translateY(-300px);
+}
+
+.products-enter-active,
+.products-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+
+.products-enter-from {
+  opacity: 0;
+  transform: translateX(300px);
+}
+
+.products-leave-to {
+  opacity: 0;
+  transform: translateX(-300px);
 }
 </style>
