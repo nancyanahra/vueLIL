8667a639087bcc581add5a284b697f0a6ee3aaa4
nancyanahra <nancy.t.nahra@gmail.com>
2024-10-28 16:41:06 -0500

    added search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,37 @@
-<template>SearchBar</template>
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="container-fluid">
+        <div class="row">
+        <div class="col-12 d-flex justify-content-start align-items-start p-3"> 
+        <form>
+            <input v-model="inputText" type="text" id="inputField" placeholder="Type here" class="form-control">
+            <button class="btn btn-primary" @click="search"> Search</button>
+        </form>
+      
+
+        </div>
+
+  </div>
+  </div>
+
+
+</template>
 
 <script>
-export default {};
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+
+  methods: {
+    search() {
+      this.$emit("search", this.inputText);
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="container">
+    <search-bar :products="filteredProducts" @search="handleSearch" />
     <range-selector :products="filteredProducts" v-model="max" />
-
+    <button @click="refresh" class="btn btn-primary">Refresh</button>
     <transition-group name="products" appear>
       <div
         v-for="item in filteredProducts"
@@ -14,28 +15,36 @@
     </transition-group>
   </section>
 </template>
-
 <script>
 import Product from "@/components/Product";
 import RangeSelector from "@/components/RangeSelector";
+import SearchBar from "@/components/SearchBar";
 export default {
   name: "HomeView",
   data: function () {
-    return {
-      max: 50,
-      cart: [],
-      displayCart: false,
-    };
+    return { max: 50, cart: [], displayCart: false, inputText: "" };
   },
   props: ["products"],
-  components: {
-    Product,
-    RangeSelector,
+  components: { Product, RangeSelector, SearchBar },
+  methods: {
+    handleSearch(inputText) {
+      console.log("Search Input: ", inputText);
+      this.inputText = inputText;
+    },
+    refresh() {
+      this.inputText = "";
+      this.max = 50;
+    },
   },
-
   computed: {
     filteredProducts() {
-      return this.products.filter((item) => item.price < Number(this.max));
+      return this.products.filter((item) => {
+        const matchesPrice = item.price < Number(this.max);
+        const matchesName = item.name
+          .toLowerCase()
+          .includes(this.inputText.toLowerCase());
+        return matchesPrice && matchesName;
+      });
     },
   },
 };
